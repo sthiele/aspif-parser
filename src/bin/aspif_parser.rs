bb8b62bcfa8c787e2f224c3a4a04ce46b068aa4c
Sven Thiele <sthiele78@gmail.com>
2019-12-11 19:06:36 +0100

    use Display instead of Debug

--- src/bin/aspif_parser.rs
+++ src/bin/aspif_parser.rs
@@ -7,7 +7,7 @@ fn main() {
     match aspif_parser::aspif_program::<VerboseError<&str>>(&buf) {
         Ok((_, result)) => println!("{:?}", result),
         Err(Err::Error(e)) | Err(Err::Failure(e)) => {
-            println!("Failed to parse aspif: {:?}", convert_error(&buf, e))
+            println!("Failed to parse aspif: {}", convert_error(&buf, e))
         }
         Err(e) => println!("Failed to parse aspif: {:?}", e),
     }
